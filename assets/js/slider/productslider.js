84fb33549a98bca78d11901039b7e575bf5116e3
it27427 <istiak.tushar.it@gmail.com>
2023-12-06 13:58:49 +0600

    productdetails-slider added

---
+++ assets/js/slider/productslider.js
@@ -0,0 +1,35 @@
+const galleryTop = new Swiper(".gallery-top", {
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: true,
+  },
+  loop: true,
+  speed: 750,
+  spaceBetween: 0,
+  pagination: false,
+  navigation: false,
+  parallax: true,
+  slidesPerView: 1,
+  centeredSlides: true,
+  grabCursor: true,
+});
+
+const galleryThumbs = new Swiper(".gallery-thumbs", {
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: true,
+  },
+  loop: true,
+  speed: 750,
+  spaceBetween: 16,
+  slidesPerView: 5,
+  pagination: false,
+  navigation: false,
+  parallax: true,
+  grabCursor: true,
+  slideToClickedSlide: true,
+  watchSlidesVisibility: true,
+});
+
+galleryTop.controller.control = galleryThumbs;
+galleryThumbs.controller.control = galleryTop;
